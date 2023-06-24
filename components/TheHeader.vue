8f249fcc75a5e72dcd7a2e79bbb11e7f53c1d971
atarashiiJS <100899825+ndt2804@users.noreply.github.com>
2023-06-24 08:02:29 +0700

    fix: fix position header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,31 +1,30 @@
 <template>
-    <nav id="navbar" class="relative z-10 w-full text-neutral-800">
-        <div class="flex flex-row max-w-screen-xl px-8 mx-auto  py-4 space-x-4 xl:space-x-8 ">
-            <div class="font-mono font-bold  py-6  text-2xl text-rose-400  gap-3 space-x-8">
+    <nav id="navbar" class="relative z-10 w-full text-neutral-800 font-bold font-mono">
+        <div class="flex flex-row max-w-screen-xl px-6 mx-auto  py-3 space-x-4 xl:space-x-8 h-20 ">
+            <div class="py-3 text-2xl text-rose-400 space-x-8">
                 <NuxtLink to="/">Yuri-ko</NuxtLink>
             </div>
-            <ul class="gap-3 flex flex-1 justify-center font-mono  space-x-4 items-center">
+            <ul class="gap-3 flex flex-1 justify-center space-x-4 items-center">
                 <li>
-                    <NuxtLink to="/">Home</NuxtLink>
+                    <NuxtLink to="/">HOME</NuxtLink>
                 </li>
                 <li>
-                    <NuxtLink to="/about">Manga</NuxtLink>
+                    <NuxtLink to="/manga">MANGA</NuxtLink>
                 </li>
                 <li>
-                    <NuxtLink to="/contact">Anime</NuxtLink>
+                    <NuxtLink to="/anime">ANIME</NuxtLink>
                 </li>
                 <li>
-                    <NuxtLink to="/license">License</NuxtLink>
+                    <NuxtLink to="/license">LICENSE</NuxtLink>
                 </li>
                 <li>
-                    <NuxtLink to="/login">About</NuxtLink>
+                    <NuxtLink to="/about">ABOUT</NuxtLink>
                 </li>
             </ul>
-            <button
-                class="bg-transparent text-rose-400 font-mono font-bold py-2 px-4 my-8 mx-4 rounded-full border border-rose-400  ">
+            <button class="bg-transparent text-rose-400 py-2 px-4 my-2 rounded-full border border-rose-400 items-center ">
                 <NuxtLink to="/login">Log In</NuxtLink>
             </button>
-            <button class="bg-rose-400 hover:bg-rose-500 text-white font-mono font-bold py-2 px-4 my-8 mx-4 rounded-full ">
+            <button class="bg-rose-400 hover:bg-rose-500 text-white py-2 px-4 my-2 rounded-full ">
                 <NuxtLink to="/register"> Sign Up </NuxtLink>
             </button>
         </div>
--- components/TheHeaderCustom.vue
+++ components/TheHeaderCustom.vue
@@ -1,7 +1,7 @@
 <template>
-    <nav id="navbar" class="relative z-10 w-full text-neutral-800">
-        <div class="flex flex-row max-w-screen-xl px-8 mx-auto  py-4 space-x-4 xl:space-x-8 ">
-            <div class="font-mono font-bold  py-6  text-2xl text-rose-400  gap-3 space-x-8">
+    <nav id="navbar" class="relative z-10 w-full text-neutral-800 font-bold font-mono">
+        <div class="flex flex-row max-w-screen-xl px-6 mx-auto  py-3 space-x-4 xl:space-x-8 h-20 ">
+            <div class="py-3 text-2xl text-rose-400 space-x-8">
                 <NuxtLink to="/">Yuri-ko</NuxtLink>
             </div>
         </div>
