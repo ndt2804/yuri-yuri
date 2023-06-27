dcc2fdffcff47fe34364fcf346d945d393821b5c
atarashiiJS <100899825+ndt2804@users.noreply.github.com>
2023-06-27 23:19:22 +0700

    test: test sv api backend

--- pages/license.vue
+++ pages/license.vue
@@ -4,81 +4,29 @@
     </div>
     <div class="container mx-auto px-6">
         <ul class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            <li
-                class="relative transform hover:scale-105 transition ease-in-out duration-500 rounded-2xl h-fit shadow-md bg-zinc-100 overflow-hidden  dark:bg-zinc-700 hover:shadow-lg">
-                <a>
-                    <img class="relative h-full w-full object-cover object-top rounded-md shadow-lg" src="
+            <div v-for="post in posts">
+                <li
+                    class="relative transform hover:scale-105 transition ease-in-out duration-500 rounded-2xl h-fit shadow-md bg-zinc-100 overflow-hidden  dark:bg-zinc-700 hover:shadow-lg">
+                    <a>
+                        <img class="relative h-full w-full object-cover object-top rounded-md shadow-lg" src="
                     " />
-                    <div class="p-6 font-bold">
-                        <p>
-                            Conan
-                        </p>
-                    </div>
-                </a>
-            </li>
-            <li
-                class="relative transform hover:scale-105 transition ease-in-out duration-500 rounded-2xl h-fit shadow-md bg-zinc-100 overflow-hidden  dark:bg-zinc-700 hover:shadow-lg">
-                <a>
-                    <img class="relative h-full w-full object-cover object-top rounded-md shadow-lg" src="
-                    " />
-                    <div class="p-6 font-bold">
-                        <p>
-                            Conan
-                        </p>
-                    </div>
-                </a>
-            </li>
-            <li
-                class="relative transform hover:scale-105 transition ease-in-out duration-500 rounded-2xl h-fit shadow-md bg-zinc-100 overflow-hidden  dark:bg-zinc-700 hover:shadow-lg">
-                <a>
-                    <img class="relative h-full w-full object-cover object-top rounded-md shadow-lg" src="
-                    " />
-                    <div class="p-6 font-bold">
-                        <p>
-                            Conan
-                        </p>
-                    </div>
-                </a>
-            </li>
-            <li
-                class="relative transform hover:scale-105 transition ease-in-out duration-500 rounded-2xl h-fit shadow-md bg-zinc-100 overflow-hidden  dark:bg-zinc-700 hover:shadow-lg">
-                <a>
-                    <img class="relative h-full w-full object-cover object-top rounded-md shadow-lg" src="
-                    " />
-                    <div class="p-6 font-bold">
-                        <p>
-                            Conan
-                        </p>
-                    </div>
-                </a>
-            </li>
-            <li
-                class="relative transform hover:scale-105 transition ease-in-out duration-500 rounded-2xl h-fit shadow-md bg-zinc-100 overflow-hidden  dark:bg-zinc-700 hover:shadow-lg">
-                <a>
-                    <img class="relative h-full w-full object-cover object-top rounded-md shadow-lg" src="
-                   " />
-                    <div class="p-6 font-bold">
-                        <p>
-                            Conan
-                        </p>
-                    </div>
-                </a>
-            </li>
-            <li
-                class="relative transform hover:scale-105 transition ease-in-out duration-500 rounded-2xl h-fit shadow-md bg-zinc-100 overflow-hidden  dark:bg-zinc-700 hover:shadow-lg">
-                <a>
-                    <img class="relative h-full w-full object-cover object-top rounded-md shadow-lg" src="
-                   " />
-                    <div class="p-6 font-bold">
-                        <p>
-                            Conan
-                        </p>
-                    </div>
-                </a>
-            </li>
+                        <div class="p-6 font-bold">
+
+                            <p>
+                                {{ post.name }}
+                            </p>
+                        </div>
+                    </a>
+                </li>
+            </div>
+
 
         </ul>
 
 
     </div>
 </template>
+<script setup>
+const { data: posts } = await useFetch('https://mihora.onrender.com/api/license', {
+})
+</script>
