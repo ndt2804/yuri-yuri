6bae3a042de8010777a4de9b3c92231241536b95
atarashiiJS <100899825+ndt2804@users.noreply.github.com>
2023-07-07 17:05:12 +0700

    refactor: update notication web

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,5 @@
 <template>
-    <h1>content nè</h1>
+    <div class="mb-12 flex h-96 items-center justify-center">
+        <h2 class="text-3xl font-bold">Đang trong quá trình phát triển </h2>
+    </div>
 </template>
