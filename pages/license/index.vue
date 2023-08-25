f595e6ba4537f486a432d40488bf47cdc6c82e1f
ndt2804 <100899825+ndt2804@users.noreply.github.com>
2023-08-25 20:11:58 +0700

    fix: fix filter form

--- pages/license/index.vue
+++ pages/license/index.vue
@@ -1,8 +1,87 @@
 <template>
     <div>
+        <div class="p-4 bg-white shadow rounded">
+            <h2 class="text-lg font-semibold mb-2">Filter Books</h2>
+            <form @submit.prevent="applyFilter">
+                <div class="mb-4">
+                    <label class="block mb-1 font-medium">Category</label>
+                    <div class="relative">
+                        <button type="button" @click="toggleCategoryDropdown" class="w-full p-2 border rounded text-left">
+                            {{ selectedCategory.length === 0 ? 'Select Category' : selectedCategoryText }}
+                        </button>
+                        <div v-if="isCategoryDropdownOpen" class="absolute w-full mt-2 p-2 bg-white border rounded shadow">
+                            <label class="flex items-center">
+                                <input type="checkbox" v-model="selectedCategory" value="light novel" class="mr-2" />
+                                Light Novel
+                            </label>
+                            <label class="flex items-center">
+                                <input type="checkbox" v-model="selectedCategory" value="manga" class="mr-2" />
+                                Manga
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-4">
+                    <label class="block mb-1 font-medium">Category</label>
+                    <div class="relative">
+                        <button type="button" @click="toggleCategoryDropdown" class="w-full p-2 border rounded text-left">
+                            {{ selectedCategory.length === 0 ? 'Select Category' : selectedCategoryText }}
+                        </button>
+                        <div v-if="isCategoryDropdownOpen" class="absolute w-full mt-2 p-2 bg-white border rounded shadow">
+                            <label class="flex items-center">
+                                <input type="checkbox" v-model="selectedCategory" value="light novel" class="mr-2" />
+                                Light Novel
+                            </label>
+                            <label class="flex items-center">
+                                <input type="checkbox" v-model="selectedCategory" value="manga" class="mr-2" />
+                                Manga
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <!-- Tương tự cho phần Publisher -->
+                <!-- ... -->
+                <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded">Apply Filter</button>
+            </form>
+        </div>
+        <!-- Hiển thị danh sách sách dựa trên bộ lọc -->
+        <!-- Sử dụng v-for để lặp qua licenses và hiển thị thông tin sách -->
+    </div>
+
+    <div class="flex items-center justify-center p-4 bg-rose-200 text-dark-200">
+        <button id="dropdownDefault" data-dropdown-toggle="dropdown"
+            class="bg-primary-700 bg-amber-400	 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            type="button">
+            Filter by category
+            <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+            </svg>
+        </button>
+
+        <!-- Dropdown menu -->
+        <div id="dropdown" class="z-10  w-56 p-3 bg-white rounded-lg shadow dark:bg-gray-700">
+            <h6 class="mb-3 text-sm font-medium text-gray-900 ">
+                Category
+            </h6>
+            <ul v-for="license in  licenses" :key="license.id" class="space-y-2 text-sm" aria-labelledby="dropdownDefault">
+                <li class="flex items-center">
+                    <input id="apple" type="checkbox" value=""
+                        class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+
+                    <label for="apple" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+                        {{
+                            license.category
+                        }} </label>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div>
         <div class="mb-12 flex h-96 items-center justify-center bg-zinc-100">
             <h1 class="container px-6 text-center text-4xl font-bold">Thông tin bản quyền</h1>
         </div>
+
         <div class="container flex flex-wrap justify-items items-center justify-between align-middle space-y-1 mx-auto">
             <form
                 class="flex items-center flex-1 snap-x snap-mandatory gap-6 overflow-x-scroll  sm:overflow-x-hidden sm:px-6 pb-5 font-lexend text-2xl font-bold flex items-center gap-3 underline decoration-tanaamber-400 decoration-[.2rem] underline-offset-[.2rem]">
@@ -26,6 +105,37 @@
                 <div class="basis-56 lg:basis-72">
                     <div class="top-6 space-y-3 sm:sticky">
                         <h3 class="font-kanit text-2xl">Fillter</h3>
+                        <form @submit.prevent="applyFilter">
+                            <div class="mb-4">
+                                <label class="block mb-1 font-medium">Category</label>
+                                <label class="flex items-center">
+                                    <input type="checkbox" v-model="selectedCategories" value="light novel" class="mr-2">
+                                    Light Novel
+                                </label>
+                                <label class="flex items-center">
+                                    <input type="checkbox" v-model="selectedCategories" value="manga" class="mr-2">
+                                    Manga
+                                </label>
+                            </div>
+                            <div class="mb-4">
+                                <label class="block mb-1 font-medium">Publisher</label>
+                                <label class="flex items-center">
+                                    <input type="checkbox" v-model="selectedPublishers" value="IPM" class="mr-2">
+                                    IPM
+                                </label>
+                                <label class="flex items-center">
+                                    <input type="checkbox" v-model="selectedPublishers" value="NXB Trẻ" class="mr-2">
+                                    NXB Trẻ
+                                </label>
+                                <label class="flex items-center">
+                                    <input type="checkbox" v-model="selectedPublishers" value="Kim Đồng" class="mr-2">
+                                    Kim Đồng
+                                </label>
+                            </div>
+                            <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded">Apply Filter</button>
+                        </form>
+
+
                         <div data-headlessui-state=""><button class="mb-3 flex w-full items-center justify-between"
                                 id="headlessui-disclosure-button-:r0:" type="button" aria-expanded="false"
                                 data-headlessui-state="">
@@ -37,7 +147,8 @@
                                     </path>
                                 </svg>
                             </button></div>
-                        <div data-headlessui-state=""><button class="mb-3 flex w-full items-center justify-between"
+                        <div data-headlessui-state="">
+                            <button class="mb-3 flex w-full items-center justify-between"
                                 id="headlessui-disclosure-button-:r1:" type="button" aria-expanded="false"
                                 data-headlessui-state="">
                                 <h3 class="mb-1">Category</h3><svg stroke="currentColor" fill="currentColor"
@@ -47,7 +158,8 @@
                                         d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z">
                                     </path>
                                 </svg>
-                            </button></div>
+                            </button>
+                        </div>
                         <div data-headlessui-state=""><button class="mb-3 flex w-full items-center justify-between"
                                 id="headlessui-disclosure-button-:r2:" type="button" aria-expanded="false"
                                 data-headlessui-state="">
@@ -60,6 +172,7 @@
                                 </svg>
                             </button></div>
                     </div>
+
                 </div>
 
                 <div
@@ -99,7 +212,33 @@
     </div>
 </template>
 <script setup lang="ts">
+
 const client = useSupabaseClient()
 const { data: licenses } = await useAsyncData('license', async () =>
     client.from('license').select('id, name, publisher, category, created_at, image_li, slug').order('created_at'), { transform: result => result.data })
+const selectedCategories = ref([]);
+const selectedPublishers = ref([]);
+const selectedPublisher = ref('');
+
+const applyFilter = () => {
+
+};
+const selectedCategory = ref([]);
+const isCategoryDropdownOpen = ref(false);
+
+
+
+
+
+
+const toggleCategoryDropdown = () => {
+    isCategoryDropdownOpen.value = !isCategoryDropdownOpen.value;
+};
+
+const selectedCategoryText = computed(() => {
+    if (selectedCategory.value.length === 0) {
+        return 'Select Category';
+    }
+    return selectedCategory.value.join(', ');
+});
 </script>
